4098568774d28a2600a2ce72258e558449d3b00f
IbrahimElmahy <hemaelmahy12@gmail.com>
2025-04-30 22:21:11 +0300

    ahmed

---
+++ Graduation Project/Models/HomePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Graduation_Project.Models
+{
+    public class HomePageViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> FeaturedProducts { get; set; }
+        public List<Product> BestSellingProducts { get; set; }
+        public List<Promotion> Promotions { get; set; }
+    }
+}
